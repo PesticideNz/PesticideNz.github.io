b9483c3ce6e48cbfdfd3f9e8c786cedd45d31a2a
O L <pesticidenz@gmail.com>
2021-07-31 20:34:33 +1200

    open first then set request header

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Http = new XMLHttpRequest();
 const url = 'https://apis.metroinfo.co.nz/rti/siri/v1/sm?stopcode=52894 HTTP/1.1';
 const key = '8101ac37658242a4b1cf6cef9d013036';
-Http.setRequestHeader("Ocp-Apim-Subscription-Key",key);
+//Failed to execute 'setRequestHeader' on 'XMLHttpRequest': The object's state must be OPENED.
 Http.open("GET", url);
+
+Http.setRequestHeader("Ocp-Apim-Subscription-Key",key);
+
 Http.send();
 console.log('Hello');
 function showResult(){
