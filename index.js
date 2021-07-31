857b31c113d87848e851638071f08810bda1f55f
O L <pesticidenz@gmail.com>
2021-07-31 20:13:34 +1200

    test initial index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const Http = new XMLHttpRequest();
+const url = 'https://apis.metroinfo.co.nz/rti/siri/v1/sm?stopcode=52894 HTTP/1.1';
+const key = '8101ac37658242a4b1cf6cef9d013036';
+Http.setRequestHeader("Ocp-Apim-Subscription-Key",key);
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
